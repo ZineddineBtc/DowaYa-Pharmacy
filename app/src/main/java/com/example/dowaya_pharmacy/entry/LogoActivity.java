71070acd2be85d0a66c962f7249cfde96e4b008c
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-08-26 00:57:23 +0100

    First commit

---
+++ app/src/main/java/com/example/dowaya_pharmacy/entry/LogoActivity.java
@@ -0,0 +1,16 @@
+package com.example.dowaya_pharmacy.entry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dowaya_pharmacy.R;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
